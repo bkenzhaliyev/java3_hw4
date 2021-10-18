94923e88c20203107805650156abf9db03351d8a
bkenzhaliyev <bkenzhaliyev@gmail.com>
2021-10-18 22:43:43 +0600

    Initial commit

---
+++ src/hw4_1/Words.java
@@ -0,0 +1,14 @@
+package hw4_1;
+
+public class Words extends Thread {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    /*1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз
+         (порядок – ABСABСABС).
+         Используйте wait/notify/notifyAll.//
+     */
+    public static void main(String[] args) {
+    
+    }
+}
